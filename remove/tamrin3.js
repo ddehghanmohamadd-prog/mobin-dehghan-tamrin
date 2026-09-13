fda1f6c409ec60728a4305d841e7964c996fd85d
ddehghanmohamadd-prog <ddehghanmohamadd@gmail.com>
2026-09-14 02:15:29 +0800

    Delete remove directory

--- remove/tamrin3.js
+++
@@ -1,8 +0,0 @@
-function removeduplicates(array){
-    let result = [];
-    for (item of array){
-        if(!result.includes(item)){result.push(item)}
-        }
-        return result
-}
-console.log(removeduplicates([1,2,3,4,5,4,2,1]));
